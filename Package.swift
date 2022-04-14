eb3c7b825cfa9a62935e10aaca71a8e6b21688b3
lucas.arts <lucas.arts@kakaocorp.com>
2022-04-14 14:29:09 +0900

    git release 전체모듈 적용.

--- Package.swift
+++ Package.swift
@@ -35,24 +35,32 @@ let package = Package(
 //    ],
     targets: [
         .binaryTarget(name: "KakaoPartnerSDKCommon",
-                      path: "kakao-partner-ios-sdk-dynamic-xcframeworks-snapshot-2.9.2/KakaoPartnerSDKCommon.xcframework"
+                      url:"https://github.com/fecom73/private-kakao-partner-ios-sdk/releases/download/2.9.2/KakaoPartnerSDKCommon_xcframework.zip",
+                      checksum: "f08fdd6a726b8b4959edecef278d86ca0284c67350ed84e9c289507453ae7833"
         ),
+        // .binaryTarget(name: "KakaoPartnerSDKCommon",
+        //               path: "kakao-partner-ios-sdk-dynamic-xcframeworks-snapshot-2.9.2/KakaoPartnerSDKCommon.xcframework"
+        // ),
         .binaryTarget(name: "KakaoPartnerSDKAuth",
-                      path: "kakao-partner-ios-sdk-dynamic-xcframeworks-snapshot-2.9.2/KakaoPartnerSDKAuth.xcframework"
+                      url:"https://github.com/fecom73/private-kakao-partner-ios-sdk/releases/download/2.9.2/KakaoPartnerSDKAuth_xcframework.zip",
+                      checksum: "f8efce11c74a4079ac45881488ce7a2641abd8c551a4bcdae1a92d6d98da7bd0"
         ),
         .binaryTarget(name: "KakaoPartnerSDKUser",
-                      path: "kakao-partner-ios-sdk-dynamic-xcframeworks-snapshot-2.9.2/KakaoPartnerSDKUser.xcframework"
+                      url:"https://github.com/fecom73/private-kakao-partner-ios-sdk/releases/download/2.9.2/KakaoPartnerSDKUser_xcframework.zip",
+                      checksum: "b638a14fb702abc532dd7a590b8b25314f26d0fc706897babadf8b7bc99a55be"
         ),
         .binaryTarget(name: "KakaoPartnerSDKTalk",
-                      path: "kakao-partner-ios-sdk-dynamic-xcframeworks-snapshot-2.9.2/KakaoPartnerSDKTalk.xcframework"
+                      url:"https://github.com/fecom73/private-kakao-partner-ios-sdk/releases/download/2.9.2/KakaoPartnerSDKTalk_xcframework.zip",
+                      checksum: "d72f83a07a12e66853b4f4907a88ae8297b8322c67a30cca238b2a950beb8a54"
         ),
         .binaryTarget(name: "KakaoPartnerSDKLink",
-                      path: "kakao-partner-ios-sdk-dynamic-xcframeworks-snapshot-2.9.2/KakaoPartnerSDKLink.xcframework"
+                      url:"https://github.com/fecom73/private-kakao-partner-ios-sdk/releases/download/2.9.2/KakaoPartnerSDKLink_xcframework.zip",
+                      checksum: "96f7651b6771b747f16f649b6998291dcd1bfcc0bcdb4cd948605209983b9743"
         ),
         .binaryTarget(name: "KakaoPartnerSDKFriend",
-                      path: "kakao-partner-ios-sdk-dynamic-xcframeworks-snapshot-2.9.2/KakaoPartnerSDKFriend.xcframework"
-        )
- 
+                      url:"https://github.com/fecom73/private-kakao-partner-ios-sdk/releases/download/2.9.2/KakaoPartnerSDKFriend_xcframework.zip",
+                      checksum: "aea0f47b5155d9539bc5845cd086ce4a15baea1f5f900a7c4b486d42f52b8854"
+        ) 
     ],
     swiftLanguageVersions: [
         .v5
